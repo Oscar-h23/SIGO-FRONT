8d520992c5cbdce6eb1874cf0023512688407951
oscar herrera <oscarherrera@MacBook-Air-de-oscar.local>
2026-08-30 05:10:47 -0500

    url actualizada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api'
+  apiUrl: 'https://sigo-asistencia-api.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '/api'
+  apiUrl: 'https://sigo-asistencia-api.onrender.com/api'
 };
