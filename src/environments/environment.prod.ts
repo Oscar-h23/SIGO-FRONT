8ec9f06e7972e0fc36a7be04c0fe445340faf9f0
oscar herrera <oscarherrera@MacBook-Air-de-oscar.local>
2026-08-30 10:53:34 -0500

    railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sigo-asistencia-api.onrender.com/api'
+  apiUrl: 'https://sigo-back-production.up.railway.app/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://sigo-asistencia-api.onrender.com/api'
+  apiUrl: 'https://sigo-back-production.up.railway.app/api'
 };
