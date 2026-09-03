8d39e19f60e8f44bc6c7c07a523a66af8574619b
oscar herrera <oscarherrera@MacBook-Air-de-oscar.local>
2026-09-03 00:14:24 -0500

    correccion del boton de editar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://sigo-back-production.up.railway.app/api'
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://sigo-back-production.up.railway.app/api'
+  
   
 };
