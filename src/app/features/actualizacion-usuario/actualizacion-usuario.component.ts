a9b6bcc3a8e35b9f357648d7a748cdd6cb92c8a7
Oscar-h23 <136534317+Oscar-h23@users.noreply.github.com>
2026-09-14 21:29:53 -0500

    Agregar aviso temporal de actualización de usuario

---
+++ src/app/features/actualizacion-usuario/actualizacion-usuario.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizacion-usuario',
+  standalone: true,
+  templateUrl: './actualizacion-usuario.component.html',
+  styleUrl: './actualizacion-usuario.component.css'
+})
+export class ActualizacionUsuarioComponent {}
